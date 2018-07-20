769f1447d632076b34c7b78942e0438c5574241a
gibsonjc1 <40771787+gibsonjc1@users.noreply.github.com>
2018-07-20 09:52:00 -0700

    Add files via upload

---
+++ module05/exercise1/exercise1.js
@@ -0,0 +1,25 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+var firstName;
+var lastName
+
+// alert('Welcome user!');
+// firstName = prompt ('Please enter your first name');
+// lastName = prompt ('Please enter your last name');
+
+firstName = 'Josh';
+lastName = 'Gibson';
+
+
+if (hourNow > 18) {
+    greeting = 'Good evening,<br />'+firstName+' '+lastName+'!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon,<br />'+firstName+' '+lastName+'!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning,<br />'+firstName+' '+lastName+'!';
+} else {
+    greeting = 'Welcome,<br />'+firstName+' '+lastName+'!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
