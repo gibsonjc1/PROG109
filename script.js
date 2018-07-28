96a00b24fa76923f74645b9a991ac479eabd5ceb
gibsonjc1 <40771787+gibsonjc1@users.noreply.github.com>
2018-07-27 18:39:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+
+
+var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
