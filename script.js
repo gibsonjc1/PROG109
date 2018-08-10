6d3fb301211157d4fb680fe75af15479c73b7a3b
gibsonjc1 <40771787+gibsonjc1@users.noreply.github.com>
2018-08-09 21:41:34 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// Calcualate and display time in <p id="date time"> of .html
+//----------------------------------------------------------------------
+// Calculate and display time in <p id="date time"> of .html
+//----------------------------------------------------------------------
 
 var x = setInterval(displayDynamicTimes, 1000);
 // Makes the time and date update every second (or 1000 milliseconds).
@@ -20,9 +22,7 @@ function displayDynamicTimes () {
 			date = dateTime.getDate() + "<sup>th</sup>";
 		}
 		
-		// Removes the seconds and the second colon from the the time.  Example: 8:23:45 AM => 8:23 AM
-//		var secondColon = dateTime.toLocaleTimeString().lastIndexOf(':');
-		var time = dateTime.toLocaleTimeString()//.substring(0,secondColon) + dateTime.toLocaleTimeString().substring(secondColon+3,dateTime.toLocaleTimeString().length);
+		var time = dateTime.toLocaleTimeString();
 	
 	return (dayName[dateTime.getDay()] + ", " + monthName[dateTime.getMonth()] + " " +date + " " + dateTime.getFullYear() + " at " + time);
 	}
@@ -34,6 +34,97 @@ function displayDynamicTimes () {
 	
 }
 //----------------------------------------------------------------------
+// Validates the user form on the contact me page
+//----------------------------------------------------------------------
+
+function Validate() {
+	
+	var formIsReady = [false, false, false];
+	
+	var name = document.getElementById("nameLoc").value;
+	var email = document.getElementById("emailLoc").value;
+	var comments = document.getElementById("commentsLoc").value;
+	
+	var nameError;
+	var emailError;
+	var commentsError;
+	
+	
+	function rightLength (localWord) {
+		if (localWord === "" || localWord === null)
+			return " required.</p>";
+		else
+			return "";
+	}
+	
+//----------NAME----------
+	nameError = rightLength (name);
+	
+	if (nameError !== "") {
+		nameLoc.style.background = "salmon";
+		document.getElementById('nameError').innerHTML = '<p><img src="images/warning.png" alt="Warning Icon" title="Warning Icon" height="30" /> Your name is' + nameError;
+		formIsReady [0] = false;
+	} else if (!/^[a-zA-Z\s-]+$/.test(name)) {
+		nameLoc.style.background = "salmon";
+		document.getElementById('nameError').innerHTML = '<p><img src="images/warning.png" alt="Warning Icon" title="Warning Icon" height="30" /> Your name can only contain letters, dashes (-) or spaces.</p>';
+		formIsReady [0] = false;
+	} else {
+		nameLoc.style.background = "white";
+		document.getElementById('nameError').innerHTML = "";
+		formIsReady [0] = true;
+	}
+	
+//----------EMAIL comments----------
+	
+/*
+This is based on the rules from
+https://social.technet.microsoft.com/Forums/exchange/en-US/69f393aa-d555-4f8f-bb16-c636a129fc25/what-are-valid-and-invalid-email-comments-characters
+where only the domain name is validated as following hostname rules.
+*/
+	emailError = rightLength (email);
+	
+	var atLoc = email.lastIndexOf("@");	
+	var emailLen = email.length;
+	var domain = email.substring(atLoc + 1,emailLen);
+		
+	if (emailError !== "") {
+		emailLoc.style.background = "salmon";
+		document.getElementById('emailError').innerHTML = '<p><img src="images/warning.png" alt="Warning Icon" title="Warning Icon" height="30" /> Your email address is ' + emailError;
+		formIsReady [1] = false;
+	} else if (atLoc <= 0 || !/^[\w.-]+$/.test(domain) || domain.lastIndexOf(".") <= 0) {
+		emailLoc.style.background = "salmon";
+		document.getElementById('emailError').innerHTML = '<p><img src="images/warning.png" alt="Warning Icon" title="Warning Icon" height="30" /> Not a valid email address.</p>';
+		formIsReady [1] = false;
+	} else {
+		emailLoc.style.background = "white";
+		document.getElementById('emailError').innerHTML = "";
+		formIsReady [1] = true;
+	}
+	
+	
+//----------COMMENTS----------
+	commentsError = rightLength (comments);
+	
+	if (commentsError !== "") {
+		commentsLoc.style.background = "salmon";
+		document.getElementById('commentsError').innerHTML = '<p><img src="images/warning.png" alt="Warning Icon" title="Warning Icon" height="30" /> Comments are' + commentsError;
+		formIsReady [2] = false;
+	} else {
+		commentsLoc.style.background = "white";
+		document.getElementById('commentsError').innerHTML = "";
+		formIsReady [2] = true;
+	}
+	
+//----------TO CHECK THAT EVERY REQUIRED FIELD IS READY----------
+	for (i = 0; i < 3; i++) {
+		if (!formIsReady [i])
+			return false;
+	}
+	
+	return true;
+  }
+
+
 
 //----------------------------------------------------------------------
 
